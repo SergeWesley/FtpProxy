9d65f27698b5e692dba6199c0a728ea4341d7967
Apedo-Amah Wesley <wesley.apedoamah.etu@univ-lille.fr>
2025-02-27 06:17:50 +0100

    Met en place le squelette du projet basé sur Api Platform + premieres routes exposées

---
+++ src/Entity/FtpServer.php
@@ -0,0 +1,69 @@
+<?php 
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\GetCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Put(), 
+        new Delete() 
+    ]
+)]
+class FtpServer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    private string $alias;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
+    private string $host;
+
+    public function getId() : int
+    {
+        return $this->id;
+    }
+
+    // Getter et Setter pour $alias
+    public function getAlias(): string
+    {
+        return $this->alias;
+    }
+
+    public function setAlias(string $alias): self
+    {
+        $this->alias = $alias;
+
+        return $this;
+    }
+
+    // Getter et Setter pour $host
+    public function getHost(): string
+    {
+        return $this->host;
+    }
+
+    public function setHost(string $host): self
+    {
+        $this->host = $host;
+
+        return $this;
+    }
+}
